ca97abaaf93b4ba8ee1d8c1f42f57883dcacab8c
Savanna Marie <savannamarie@gmail.com>
2021-07-14 09:29:46 -0500

    User input

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -33,8 +34,12 @@ public class RegisterServlet extends HttpServlet {
             return;
         }
 
+        String hash = Password.hash(password);
+        String confirmHash = Password.hash(password);
+        System.out.println(hash + "\n" + confirmHash);
+
         // create and save a new user
-        User user = new User(username, email, password);
+        User user = new User(username, email, hash);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
     }
